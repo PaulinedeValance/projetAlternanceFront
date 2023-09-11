6cc36ad52312c18d3ba35fc47ee8d43ba83d0db4
PaulinedeValance <pauline.de.valance@gmail.com>
2023-09-11 15:45:40 +0200

    route pour gameGenerator

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -12,7 +12,7 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
                 class="icon-camera" />Ma collection</router-link></li>
           <li class="navbar-link"><router-link to="/userwishlist"><font-awesome-icon :icon="'list'"
                 class="icon-camera" />Wish list</router-link></li>
-          <li><a href="#"><font-awesome-icon :icon="'rotate-right'" class="icon-camera" />Générateur</a></li>
+          <li><a href="gamesgenerator"><font-awesome-icon :icon="'rotate-right'" class="icon-camera" />Générateur</a></li>
         </ul>
       </div>
       <div class="page-container">
