f5406f773a74128406e52b6ec05a785be24487d1
PaulinedeValance <pauline.de.valance@gmail.com>
2023-09-18 10:49:25 +0200

    maj

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import SignUpForm from "../components/SignUpForm.vue";
+import Footer from "@/components/Footer.vue";
+
 </script>
 
 <template>
+  <!-- <h1>Inscrivez-vous</h1> -->
   <SignUpForm />
+  <Footer />
 </template>
