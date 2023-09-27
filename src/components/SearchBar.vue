71b0cbbd22cb8a3c5fff4c820568fa9ca975ae6e
PaulinedeValance <pauline.de.valance@gmail.com>
2023-09-27 14:53:24 +0200

    design de la barre de recherche

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -3,14 +3,35 @@ import { ref } from 'vue';
 
 const searchQuery = ref('');
 
-const search = () => {
+const emits = defineEmits(['search']);
 
+const performSearch = () => {
+  emits('search', searchQuery.value); // J'émets l'événement 'search' avec la valeur de searchQuery
 };
 </script>
 
 <template>
   <div>
-    <input v-model="searchQuery" @input="search" type="text" placeholder="Rechercher..." />
-    <button @click="search">Rechercher</button>
+    <input class="search-bar" v-model="searchQuery" @input="performSearch" placeholder="Rechercher un jeu..." />
   </div>
 </template>
+<style>
+.search-bar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid #218e76ce;
+  border-radius: 25px;
+  padding: 5px;
+  transition: all 0.2s;
+  padding-left: 15px;
+  margin-top: 15px;
+
+}
+
+.search-bar:focus {
+  border: 4px solid #218e76ce;
+  outline: none;
+  color: #218e76ce;
+}
+</style>
