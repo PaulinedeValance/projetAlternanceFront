b240b8fbe66018490e230ba2f98ae050a364e19a
PaulinedeValance <pauline.de.valance@gmail.com>
2023-10-15 17:36:10 +0200

    refacto navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,38 +3,55 @@
 
 <template>
   <nav>
-    <ul class="navbar-display">
-      <li class="navbar-link">
-        <router-link to="/jeux">Les jeux</router-link>
-      </li>
-      <li class="navbar-link">
-        <router-link to="/contact">Contact</router-link>
-      </li>
-      <li class="navbar-link">
-        <router-link to="/signup">Créer un compte</router-link>
-      </li>
-    </ul>
+    <div class="navbar-container">
+      <ul class="nav-link">
+        <li class="navbar-link">
+          <router-link to="/contact">Contact</router-link>
+        </li>
+        <li class="navbar-link">
+          <router-link to="/signup">Créer un compte</router-link>
+        </li>
+      </ul>
+      <img src="/images/logo-bleu.png" alt="Logo" class="logo-navbar" />
+    </div>
   </nav>
 </template>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Bellota+Text:wght@700&family=Cabin+Sketch:wght@700&family=Didact+Gothic&family=Handlee&family=Londrina+Shadow&family=Pacifico&family=Patrick+Hand+SC&family=Rampart+One&family=Sue+Ellen+Francisco&display=swap');
 
-.navbar-display {
+.navbar-container {
   display: flex;
-  justify-content: space-around;
-  align-items: center;
+  flex-direction: column;
+  align-items: flex-end;
+  margin-top: 10px;
+
+}
+
+.nav-link {
+  list-style: none;
+  padding: 0;
+  font-family: 'Bellota Text', cursive;
+  padding-top: 20px;
+  padding-right: 25px;
 }
 
 .navbar-link {
-  list-style: none;
-  padding: 20px;
-  font-family: 'Bellota Text', cursive;
+  margin-right: 40px;
+}
+
+.logo-navbar {
+  width: 40%;
+  margin-right: 40px;
 }
 
 a {
   text-decoration: none;
-  color: black;
+  color: #2B4573;
   font-size: 22px;
+}
+
+.nav-link {
+  display: flex;
 }
 </style>
